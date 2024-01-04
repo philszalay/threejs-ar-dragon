c3b96cb7e887f2225493396e91efd1bc196c5ece
Phil Szalay <philszalay@aol.de>
2024-01-04 18:37:08 +0100

    Try out some models

--- src/script.js
+++ src/script.js
@@ -2,9 +2,15 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { ARButton } from 'three/examples/jsm/webxr/ARButton'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
+import dragon from '../assets/models/dragon.glb'
+import cathedral from '../assets/light/cathedral.hdr'
 
 export default class ThreeJsDraft {
-  constructor () {
+  constructor() {
     /**
      * Variables
      */
@@ -12,6 +18,13 @@ export default class ThreeJsDraft {
     this.width = window.innerWidth
     this.height = window.innerHeight
     this.devicePixelRatio = window.devicePixelRatio
+    this.clock = new THREE.Clock()
+
+    this.uniforms = {
+      u_resolution: { type: 'v2', value: new THREE.Vector2(this.width, this.height) },
+      u_time: { type: 'f', value: 0.0 },
+      u_texture: { value: null }
+    }
 
     /**
      * Scene
@@ -28,10 +41,14 @@ export default class ThreeJsDraft {
      * Renderer
      */
     this.renderer = new THREE.WebGLRenderer({
-      canvas: this.canvas
+      canvas: this.canvas,
+      alpha: true
     })
     this.renderer.setSize(this.width, this.height)
     this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2))
+    this.renderer.xr.enabled = true;
+    document.body.appendChild(ARButton.createButton(this.renderer));
+    this.renderer.setAnimationLoop(this.animate.bind(this));
 
     /**
      * Controls
@@ -88,19 +105,54 @@ export default class ThreeJsDraft {
      * Objects
      */
     this.addObjects()
-
-    this.renderer.render(this.scene, this.camera)
-    /**
-     * Animation Loop
-     */
-    this.animate()
   }
 
-  loadAssets () {
-    // const textureLoader = new THREE.TextureLoader(this.loadingManager)
+  loadAssets() {
+    const loader = new GLTFLoader(this.loadingManager);
+
+    loader.load(dragon, (gltf) => {
+
+      console.log(gltf.scene)
+
+      gltf.scene.traverse((object) => {
+        if (object.isMesh) {
+          object.material.envMapIntensity = 5
+          console.log(object.material);
+        }
+      })
+
+      this.scene.add(gltf.scene)
+
+      gltf.scene.scale.set(.1, .1, .1)
+
+      this.mixer = new THREE.AnimationMixer(gltf.scene)
+
+      const animationAction = this.mixer.clipAction(gltf.animations[0]).play()
+    },
+      // called while loading is progressing
+      function (xhr) {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+      },
+      // called when loading has errors
+      function (error) {
+
+        console.log('An error happened');
+
+      })
+
+    const hdriLoader = new RGBELoader(this.loadingManager)
+    let generator = new THREE.PMREMGenerator(this.renderer);
+
+    hdriLoader.load(cathedral, (texture) => {
+      texture.mapping = THREE.EquirectangularReflectionMapping
+      let envmap = generator.fromEquirectangular(texture);
+      this.scene.environment = envmap.texture
+    });
   }
 
-  addHelpers () {
+  addHelpers() {
     const axisHelper = new THREE.AxesHelper(3)
     this.scene.add(axisHelper)
 
@@ -108,20 +160,18 @@ export default class ThreeJsDraft {
     document.body.appendChild(this.stats.dom)
   }
 
-  addObjects () {
-    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-    const cubeMaterial = new THREE.MeshBasicMaterial({
-      color: 0x00ff00
-    })
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-    this.scene.add(cube)
+  addObjects() {
   }
 
-  animate () {
+  animate() {
     this.orbitControls.update()
     this.stats.update()
+
+    var delta = this.clock.getDelta();
+    if (this.mixer) this.mixer.update(delta);
+
+    this.uniforms['u_time'].value += 0.01
     this.renderer.render(this.scene, this.camera)
-    window.requestAnimationFrame(this.animate.bind(this))
   }
 }
 
